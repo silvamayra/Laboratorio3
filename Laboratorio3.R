d00de3250e777dbf3b840811fc15d35a78a46460
Mayra <sil17276@uvg.edu.gt>
2019-08-28 12:54:16 -0600

    quite na, matriz de corelacion

--- Laboratorio3.R
+++ Laboratorio3.R
@@ -66,7 +66,7 @@ class(data)
 #Quitando las columnas llenas de NA
 dataClean <- data[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,16,17,18,19,20,21,22,24,25)]
 
-
+dataClean[is.na(dataClean)] <- 0
 
 #Exploracion rapida
 
