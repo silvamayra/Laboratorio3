58bd1e6ce21c3df19334e17c85a9fed7acca93b0
Mayra <sil17276@uvg.edu.gt>
2019-08-28 13:48:38 -0600

    Auto stash before merge of "master" and "origin/master"
    cambios de normalidad

--- Laboratorio3.R
+++ Laboratorio3.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 ########################################
 ## Universidad del Valle de Guatemala ##
@@ -157,3 +158,114 @@ auto.arima(data$Diesel)
 fit <- arima(log(data$Diesel), c(0, 1, 1),seasonal = list(order = c(0, 1, 1), period = 1))
 
 
+=======
+
+########################################
+## Universidad del Valle de Guatemala ##
+## Laboratorio 2: Data Science        ##
+## Autores:                           ##
+##    Mayra Silva                     ##
+##    Odalis Reyes                    ##           
+##    Ivan Maldonado                  ##
+########################################
+
+
+
+# Instalación de paquetes
+#install.packages("rela")
+#install.packages("psych")
+#install.packages("FactoMineR")
+#install.packages("corrplot")
+#install.packages("cluster")
+#install.packages("fpc")
+#install.packages("NbClust")
+#install.packages("factoextra")
+#install.packages("REdaS")
+#install.packages("arules")
+#install.packages("ggplot2")
+#install.packages("ggpubr")
+#install.packages("ggmap")
+#install.packages("arulesViz")
+#install.packages("dplyr")
+#install.packages("caret")
+#install.packages("class")
+#install.packages("e1071")
+#install.packages("fitdistrplus")
+
+require(ggpubr) # Para mejorar la visualización gráfica
+require(tidyverse) # Para explotar, manipular y visualizar datos que comparten info
+require(corrplot) # Para visualizar la matriz de correlación
+require(cluster) #Para calcular la silueta
+library(fpc) # Para hacer el plotcluster
+library(NbClust) # Para determinar el numero de clusters optimo
+library(factoextra) # Para hacer graficos bonitos de clustering
+library(rela) # Para poder utilizar paf()
+library(psych) # Para poder utilizar KMO()
+library(FactoMineR)
+library(corrplot)
+library(REdaS)
+library(ggplot2) # Graficas bonitas
+library(ggpubr) # Graficas bonitas x2
+#library(ggmap)
+library(arules) # Reglas de asociacion
+library(factoextra) 
+library(arulesViz)
+library(dplyr)
+library(caret) # Muestreo estratificado
+library(class) # Para KNN
+library(e1071) # Requisito para la matriz de confusión
+library(fitdistrplus)
+
+setwd("C:/Users/smayr/Documents/Tercer año/Semestre 6/Data Science/Laboratorio 3/Laboratorio3")
+
+
+# Leyendo el dataset de csv importacion
+datos <- read.csv("datosImp.csv", TRUE, ",")
+# Volviendo el csv en un data frame
+class(datos)
+
+
+#Quitando las columnas llenas de NA
+data <- datos[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,16,17,18,19,20,21,22,24,25)]
+
+#quitando ceros
+data[is.na(data)] <- 0
+
+#Exploracion rapida
+
+summary(data)
+
+
+#-------------------------- Analisis exploratorio ------------------------
+
+
+
+corr <- cor(data)
+# Se visualiza la matriz de correlación de forma gráfica
+corrplot(corr)
+
+
+#meses de mayor venta
+
+qqnorm(data$Total)
+
+
+#Ajuste de normalidad, para la variable diesel. 
+
+descdist(data$Diesel, discrete= FALSE)
+dieselfit <- fitdist(data$Diesel,"norm") ## tomar en cuenta qq plot. 
+plot(dieselfit)
+
+
+
+#Ajuste de normalidad para variable de Regular
+
+descdist(data$GasRegular, discrete= FALSE)
+
+#Prueba de normalidad para super
+descdist(data$GasSuperior, discrete= FALSE)
+
+superfit <- fitdist(data$GasSuperior,"norm") ## tomar en cuenta qq plot. 
+plot(superfit)
+
+>>>>>>> Stashed changes
